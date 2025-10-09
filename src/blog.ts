2ce02d9c59d7bdf0c488a1d44e6906f86ed6d595
7Teen17 <dominic.siino@gmail.com>
2025-10-09 14:30:07 -0700

    feat: create blog posts

--- src/blog.ts
+++ src/blog.ts
@@ -25,3 +25,27 @@ const blogs: Blog[] = [
     slug: "second-blog-post",
   },
 ];
+
+const blogContainer = document.getElementById("blog-container");
+
+blogs.forEach((blog) => {
+  const post = document.createElement("div");
+  post.classList.add("center");
+
+  const image = document.createElement("img");
+  image.src = blog.image;
+  image.alt = blog.imageAlt;
+  post.append(image);
+
+  const title = document.createElement("h1");
+  title.textContent = blog.title;
+  post.append(title);
+
+  const description = document.createElement("h2");
+  description.textContent = blog.description;
+  post.append(description);
+
+  const date = document.createElement("p");
+  date.textContent = blog.date;
+  post.append(date);
+});
