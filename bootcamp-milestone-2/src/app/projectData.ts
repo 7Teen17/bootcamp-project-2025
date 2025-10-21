d7540ea5042adeda1fdf48866d92945caf93550e
7Teen17 <dominic.siino@gmail.com>
2025-10-21 15:45:18 -0700

    fix: image imports

--- bootcamp-milestone-2/src/app/projectData.ts
+++ bootcamp-milestone-2/src/app/projectData.ts
@@ -5,7 +5,7 @@ const projects: Project[] = [
     title: "Personal Website",
     description:
       "Created a personal website to learn HTMl and CSS for Hack4Impact",
-    image: "public/h4i_website.png",
+    image: "/h4i_website.png",
     imageAlt: "Hack 4 Impact Website",
     url: "/",
   },
@@ -13,7 +13,7 @@ const projects: Project[] = [
     title: "Color Jumper",
     description:
       "Developing an Android and iOS video game, where the player switches colors and jumps to blocks to stay alive. Made with Godot and GDScript.",
-    image: "public/bootsplash.png",
+    image: "/bootsplash.png",
     imageAlt: "Color Jumper",
     url: "https://github.com/Xavier-Royer/Color-Jumper",
   },
@@ -21,7 +21,7 @@ const projects: Project[] = [
     title: "Discord Bot: Matt",
     description:
       "Recreated Sorry! board game in Discord, complete with automated turn verification and thorough Discord integration. Used Pillow library to generate on-the-fly images of the current board based on the game state. Stored game state and other variables from all the features of the bot in a sqlite database with SQL injection prevention. Accessed external API for song quotes and random trivia.",
-    image: "public/mattbot.png",
+    image: "/mattbot.png",
     imageAlt: "Discord Bot",
     url: "https://github.com/7Teen17/TFTDiscordBot",
   },
