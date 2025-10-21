ad383494649a9763e9576f55b0a7c685ecb4610d
7Teen17 <dominic.siino@gmail.com>
2025-10-21 15:54:47 -0700

    feat: style entire preview

--- bootcamp-milestone-2/src/components/projectPreview.tsx
+++ bootcamp-milestone-2/src/components/projectPreview.tsx
@@ -6,7 +6,7 @@ import type { Project } from "@/typings/project";
 
 export default function ProjectPreview(props: Project) {
   return (
-    <div className="flex flex-row border-2 w-250 h-100 max-w-250 max-h-100 rounded-xl items-center p-5">
+    <div className="flex flex-row border-2 w-250 h-100 max-w-250 max-h-100 rounded-xl items-center p-5 bg-white m-5">
       <div className="flex-1 flex items-center justify-center">
         <Image
           src={props.image}
@@ -17,10 +17,12 @@ export default function ProjectPreview(props: Project) {
         />
       </div>
 
-      <div className="flex-2 pl-2">
-        <p className="project-name">{props.title}</p>
+      <div className="flex-2 pl-5 text-black font-(family-name:--font-geist-sans)">
+        <p className="font-bold">{props.title}</p>
         <p className="project-description">{props.description}</p>
-        <Link href={props.url}>Link to Page</Link>
+        <Link href={props.url} className="text-green-300">
+          Link to Page
+        </Link>
       </div>
     </div>
   );
